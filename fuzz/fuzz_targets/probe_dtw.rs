d6c8c453fa6ac24145cdca963b02d4c0b184bac3
Niklas Fiekas <niklas.fiekas@backscattering.de>
2024-07-22 23:53:22 +0200

    submit only legal positions in fuzz target

--- fuzz/fuzz_targets/probe_dtw.rs
+++ fuzz/fuzz_targets/probe_dtw.rs
@@ -1,6 +1,7 @@
 #![no_main]
 
 use std::{
+    collections::HashMap,
     ffi::{c_char, c_int},
     sync::Once,
 };
@@ -9,8 +10,42 @@ use antichess_tb_sys::{antichess_tb_add_path, antichess_tb_init, antichess_tb_pr
 use arbitrary::{Arbitrary, Unstructured};
 use libfuzzer_sys::fuzz_target;
 
-#[derive(Debug)]
+#[derive(Debug, Hash, Eq, PartialEq, Copy, Clone)]
 struct Square(c_int);
+
+impl Square {
+    fn from_file_and_rank(file: c_int, rank: c_int) -> Option<Square> {
+        if 0 <= file && file < 8 && 0 <= rank && rank < 8 {
+            Some(Square(file + rank * 8))
+        } else {
+            None
+        }
+    }
+
+    fn rank(self) -> c_int {
+        self.0 / 8
+    }
+
+    fn file(self) -> c_int {
+        self.0 % 8
+    }
+
+    fn pawn_pushed_from(self) -> Square {
+        Square(self.0 ^ 24)
+    }
+
+    fn pawn_pushed_to(self) -> Square {
+        Square(self.0 ^ 8)
+    }
+
+    fn left(self) -> Option<Square> {
+        Square::from_file_and_rank(self.file() - 1, self.rank())
+    }
+
+    fn right(self) -> Option<Square> {
+        Square::from_file_and_rank(self.file() - 1, self.rank())
+    }
+}
 
 impl Arbitrary<'_> for Square {
     fn arbitrary(u: &mut Unstructured<'_>) -> arbitrary::Result<Square> {
@@ -18,7 +53,7 @@ impl Arbitrary<'_> for Square {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 struct Role(c_int);
 
 impl Arbitrary<'_> for Role {
@@ -27,10 +62,24 @@ impl Arbitrary<'_> for Role {
     }
 }
 
+#[derive(Debug, Arbitrary, Eq, PartialEq)]
+struct Piece {
+    role: Role,
+    white: bool,
+}
+
+impl Piece {
+    fn pawn(white: bool) -> Piece {
+        Piece {
+            role: Role(1),
+            white,
+        }
+    }
+}
+
 #[derive(Debug, Arbitrary)]
 struct Pos {
-    white: Vec<(Square, Role)>,
-    black: Vec<(Square, Role)>,
+    board: HashMap<Square, Piece>,
     white_to_move: bool,
     ep_square: Option<Square>,
 }
@@ -48,16 +97,34 @@ fuzz_target!(|pos: Pos| {
         );
     });
 
-    let (white_squares, white_pieces): (Vec<c_int>, Vec<c_int>) = pos
-        .white
-        .into_iter()
-        .map(|(Square(s), Role(r))| (s, r))
-        .unzip();
-    let (black_squares, black_pieces): (Vec<c_int>, Vec<c_int>) = pos
-        .black
-        .into_iter()
-        .map(|(Square(s), Role(r))| (s, r))
-        .unzip();
+    let mut white_squares: Vec<c_int> = Vec::new();
+    let mut white_pieces: Vec<c_int> = Vec::new();
+    let mut black_squares: Vec<c_int> = Vec::new();
+    let mut black_pieces: Vec<c_int> = Vec::new();
+    for (square, piece) in &pos.board {
+        if piece.role == Role(1) && (square.rank() == 0 || square.rank() == 7) {
+            continue; // Do not put pawn on backrank
+        }
+
+        if piece.white {
+            white_squares.push(square.0);
+            white_pieces.push(piece.role.0);
+        } else {
+            black_squares.push(square.0);
+            black_pieces.push(piece.role.0);
+        }
+    }
+
+    let legal_ep_square = pos.ep_square.filter(|s| {
+        s.rank() == if pos.white_to_move { 5 } else { 2 }
+            && !pos.board.contains_key(s)
+            && !pos.board.contains_key(&s.pawn_pushed_from())
+            && pos.board.get(&s.pawn_pushed_to()) == Some(&Piece::pawn(!pos.white_to_move))
+            && (s.pawn_pushed_to().left().and_then(|c| pos.board.get(&c))
+                == Some(&Piece::pawn(pos.white_to_move))
+                || s.pawn_pushed_to().right().and_then(|c| pos.board.get(&c))
+                    == Some(&Piece::pawn(pos.white_to_move)))
+    });
 
     let mut dtw: c_int = 0;
     unsafe {
@@ -69,7 +136,7 @@ fuzz_target!(|pos: Pos| {
             black_pieces.as_ptr(),
             black_squares.len(),
             if pos.white_to_move { 0 } else { 1 },
-            pos.ep_square.map_or(-1, |Square(s)| s),
+            legal_ep_square.map_or(-1, |Square(s)| s),
             &mut dtw,
         );
     }
