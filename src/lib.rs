7f8586e1264e3857cf4b6f737d49e1694f6b158e
Niklas Fiekas <niklas.fiekas@backscattering.de>
2024-07-23 20:14:59 +0200

    bump 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/antichess-tb-sys/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/antichess-tb-sys/0.1.1")]
 #![forbid(unsafe_op_in_unsafe_fn)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
